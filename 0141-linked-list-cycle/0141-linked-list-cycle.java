a5a37d2346f73c5296f9d7b390ed1173694e9709
taetaetae <taetaetae_@naver.com>
2025-01-18 11:23:33 +0900

    Time: 3 ms (14.34%), Space: 44.8 MB (27.1%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /*
+    문제가 이해가 안되지만...
+    계속 순회하면서 순환이 되는 리스트인지를 확인하는 문제.
+    next Node가 있었다면 true, 끝까지 갔는데 없으면 false
+    */
+    // Set<Integer> nodeVals = new HashSet();
+    Set<ListNode> nodes = new HashSet();
+
+
+    public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+        if(nodes.contains(head)){
+            return true;
+        }
+        if(head.next == null){
+            return false;
+        }
+
+        nodes.add(head);
+        return hasCycle(head.next);
+    }
+}
