2b75684b5d580756725806f942d724f1631a1085
taetaetae <taetaetae_@naver.com>
2025-01-25 00:49:34 +0900

    Time: 13 ms (63.11%), Space: 53.1 MB (52.44%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -1,21 +1,27 @@
 class Solution {
     public int numEquivDominoPairs(int[][] dominoes) {
-        /*
+    /*
     도미노 2개쌍 중 앞뒤 같은것 포함해서 같은 쌍의 최대 조합의 수 구하기
      */
     Map<Integer, Integer> dominoMap = new HashMap<>();
-    int count = 0;
     for (int i = 0; i < dominoes.length; i++) {
       int min = Math.min(dominoes[i][0], dominoes[i][1]);
       int max = Math.max(dominoes[i][0], dominoes[i][1]);
       int key = min * 10 + max;
       if (dominoMap.containsKey(key)) {
-        count += dominoMap.get(key);
         dominoMap.put(key, dominoMap.get(key) + 1);
       } else {
         dominoMap.put(key, 1);
       }
     }
-    return count;
+
+    int sumOfValues = 0;
+    for (int value : dominoMap.values()) {
+      if (value == 1) {
+        continue;
+      }
+      sumOfValues += (value * (value - 1)) / 2;
     }
+    return sumOfValues;
+  }
 }
