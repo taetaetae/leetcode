a2a2819c0423c58dde970b28bf7dbbd0c5483e0e
taetaetae <taetaetae_@naver.com>
2025-02-08 09:57:56 +0900

    Time: 2 ms (38.02%), Space: 46.8 MB (30.55%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.java
@@ -10,24 +10,43 @@
  */
 class Solution {
     public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode currNode = list1;
-        int index = 0;
-        while (index < a - 1) {
-            currNode = currNode.next;
-            index++;
-        }
-        ListNode front = currNode;
-        while (index < b + 1) {
-            currNode = currNode.next;
-            index++;
-        }
-        ListNode rear = currNode;
-        ListNode secondListTail = list2, secondListHead = list2;
-        while (secondListTail.next != null) {
-            secondListTail = secondListTail.next;
-        }
-        front.next = secondListHead;
-        secondListTail.next = rear;
-        return list1;
+    /*
+    두 리스트 노트가 주어지고
+    첫번째 리스트 노드에서 a 번째 부터 b 번째를 두번째 리스트 노드로 교체하라는 문제
+
+    a는 1보다 크니 결국 첫번째 리스트의 헤드를 리턴하면 될것 같고.
+
+    a-1 번째를 찾고, b+1 번째를 찾아서
+
+    a -1 번째의 next를 두번째 리스트로 갈아 끼우고
+    두번째 리스트의 마지막 노드의 next를 b+1 번째로 교체
+     */
+
+    ListNode seekNode = list1;
+    ListNode headNode = null, tailNode = null;
+
+    int index = 0;
+    while (seekNode.next != null) {
+      if (index == a - 1) {
+        headNode = seekNode;
+      } else if (index == b) {
+        tailNode = seekNode.next;
+      }
+      seekNode = seekNode.next;
+      index++;
     }
+
+    if (tailNode == null) {
+      tailNode = seekNode.next;
+    }
+
+    headNode.next = list2;
+
+    seekNode = list2;
+    while (seekNode.next != null) {
+      seekNode = seekNode.next;
+    }
+    seekNode.next = tailNode;
+    return list1;
+  }
 }
