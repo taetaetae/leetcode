97ef5840f093f29bf91d07a4a5216af66f39b424
taetaetae <taetaetae_@naver.com>
2025-02-10 23:19:00 +0900

    Time: 3 ms (41.96%), Space: 41.8 MB (65.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,33 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        /*
-        여는게 나오면 스택에 넣고
-        닫는게 나오면 쌓이 맞으면 뽑고 아니면 끝
-        마지막까지 가서 비어있으면 true, 아니면 false
-        */
-    Stack<Character> stack = new Stack<>();
-    for (int i = 0; i < s.length(); i++) {
-      char c = s.charAt(i);
-      
-      if (stack.size() == 0 && (c == ')' || c == '}' || c == ']')) {
-        return false;
-      }
+        Stack<Character> stack = new Stack<>();
 
-      if (c == '(' || c == '{' || c == '[') {
-        stack.add(c);
-      } else {
-        Character peeked = stack.peek();
-        if (peeked == '(' && c == ')') {
-          stack.pop();
-        } else if (peeked == '{' && c == '}') {
-          stack.pop();
-        } else if (peeked == '[' && c == ']') {
-          stack.pop();
-        } else {
-          return false;
+        Map<Character, Character> bracketMap = new HashMap<>();
+        bracketMap.put(')', '(');
+        bracketMap.put('}', '{');
+        bracketMap.put(']', '[');
+
+        for(Character ch : s.toCharArray()){
+            if(bracketMap.containsValue(ch)){
+                stack.push(ch);
+            }else if (bracketMap.containsKey(ch)){
+                if(stack.isEmpty() || !stack.pop().equals(bracketMap.get(ch))){
+                    return false;
+                }
+            }
         }
-      }
-    }
-    return stack.size() == 0;
+        
+        return stack.isEmpty();
     }
 }
