2133f0dc7b8e7b83bcbd9eac34eb90fc2641e350
taetaetae <taetaetae_@naver.com>
2025-02-15 18:24:38 +0900

    Time: 27 ms (5.37%), Space: 45.1 MB (11.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,33 +3,15 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        Map<String, Integer> sMap = characterCountMap(s);
-        Map<String, Integer> tMap = characterCountMap(t);
-        
-        for(String key : sMap.keySet()){
-            Integer a = sMap.get(key);
-            Integer b = tMap.get(key);
-            if(a==null || b==null){
-                return false;
-            }
-            if(a.intValue() != b.intValue()){
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public Map<String, Integer> characterCountMap(String s){
-        Map<String, Integer> resultMap = new HashMap();
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
         for(int i=0 ; i<s.length() ; i++){
-            String str = String.valueOf(s.charAt(i));
-            if(resultMap.containsKey(str)){
-                resultMap.put(str, resultMap.get(str) + 1);
-            }else {
-                resultMap.put(str, 1);
-            }
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0)  + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0)  + 1);
         }
-        return resultMap;
-        
+
+        return sMap.equals(tMap);
     }
 }
