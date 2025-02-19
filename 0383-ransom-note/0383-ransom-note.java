5291ac0fd34ea775c07be20228aedf0f228ab392
taetaetae <taetaetae_@naver.com>
2025-02-19 23:46:55 +0900

    Time: 2 ms (82.64%), Space: 44.8 MB (77.23%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,33 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-/*
-    magazine을 가지고 ransomNote를 만들수 있는지에 대한 문제
-    magazine 문자는 한번만 사용이 가능하다.
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
 
-    magazine을 순회하면서 ransomNote에서 한글자씩 찾아 있으면 넘어가면서 ransomNote 에서 제거
-     */
-
-    char[] megazineCharArray = magazine.toCharArray();
-    List megazineCharList = new ArrayList<String>();
-    for (int i = 0; i < megazineCharArray.length; i++) {
-      megazineCharList.add(megazineCharArray[i]);
-    }
-
-    List<Integer> findIndexList = new ArrayList<>();
-    for (int i = 0; i < ransomNote.length(); i++) {
-      char ransomNoteChar = ransomNote.charAt(i);
-
-      int findIndex = megazineCharList.indexOf(ransomNoteChar);
-      if (findIndex >= 0) {
-        if (findIndexList.contains(findIndex)) {
-          return false;
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
-        findIndexList.add(findIndex);
-        megazineCharList.set(findIndex, -1);
-      } else {
-        return false;
-      }
+        return true;
     }
-    return true;
-  }   
-   }
+}
