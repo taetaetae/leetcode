5ff46666c8d07ef8b07ee7b95d8206f1e6923211
taetaetae <taetaetae_@naver.com>
2025-03-05 12:42:21 +0900

    Time: 0 ms (100%), Space: 42.8 MB (36.41%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,15 +15,9 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
-        return 0;
+        if(root == null){
+            return 0;
         }
-
-        // int delta = 1;
-        // if (root.left == null && root.right == null) {
-        //     return 1;
-        // }
-
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-  }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));   
+    }
 }
