cfac784d5110e37302d61599f7dbd5f3bdb46ca9
taetaetae <taetaetae_@naver.com>
2025-03-08 23:33:02 +0900

    Time: 1 ms (100%), Space: 42.2 MB (92.83%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,6 +1,6 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0) return -1;
+        // if(grid == null || grid.length == 0) return -1;
 
         for(int i=0 ; i<grid.length ; i++){
             for(int j=0 ; j<grid[0].length ; j++){
