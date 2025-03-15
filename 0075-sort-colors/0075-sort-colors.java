1c2d207283cb84282fe126d9fbd7707ad1239066
taetaetae <taetaetae_@naver.com>
2025-03-15 23:36:02 +0900

    Time: 9 ms (3.66%), Space: 42.6 MB (5.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);      
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for(int num: nums){
+            numMap.put(num, numMap.getOrDefault(num, 0) + 1);
+        }
+        System.out.println(numMap);
+
+        int index = 0;
+        for(Integer num: numMap.keySet()){
+            int count=0;
+            while(count++ < numMap.get(num)){
+                System.out.println(num);
+                nums[index++] = num;
+            }   
+        }
     }
 }
