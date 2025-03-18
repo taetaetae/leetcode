2ee7602c1214fd973d4beb1016bf070d5fbfb1e7
taetaetae <taetaetae_@naver.com>
2025-03-19 08:18:29 +0900

    Time: 5 ms (8.12%), Space: 42.8 MB (23.23%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,74 +1,39 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s.equals("")) {
-			return 0;
-		}
-		
-		// helper variables
-		int res = 0, i = 0, sign = 1;
-		
-		// get rid of whitespace
-		while (i < s.length() && s.charAt(i) == ' ') {
-			i++;
-		}
-		
-		// check for sign
-		if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-			// change if negative, iterate
-			if (s.charAt(i++) == '-') {
-				sign = -1;
-			}
-		}
-		
-		// now iterate across digits if any
-		// should only be in range 0-9
-		while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-			// check if we will go over the max
-			if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > 7)) {
-				if (sign == -1) {
-					return Integer.MIN_VALUE;
-				}
-				return Integer.MAX_VALUE;
-			}
-			
-			// update res
-			res = res * 10 + (s.charAt(i++) - '0');
-		}
-		return sign * res;
+        s = s.trim();
+        if(s.length() == 0){
+            return 0;
+        }
+
+        int sign = 1;
+        int index = 0;
+        if(s.charAt(0) == '-'){
+            sign = -1;
+            index++;
+        }else if(s.charAt(0) == '+'){
+            index++;
+        }
+
+        int result = 0;
+        while(index < s.length()){
+            char ch = s.charAt(index);
+            if(ch < '0' || ch > '9'){
+                break;
+            }
+            int digit = ch - '0';
+            if(result > (Integer.MAX_VALUE - digit) / 10 ){
+                return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            result = result * 10 + digit;
+            index++;
+        }
+        System.out.println(result);
+
+        return sign * result;
     }
 }
 
-// class Solution {
-//     public int myAtoi(String s) {
-//         s = s.trim();
-//         // 음수 체크 
-//         int sign = s.charAt(0) == '-' ? -1 : 1;
-
-//         int start = sign == -1 ? 1 : 0;
-//         int end = 0;
-//         for(end=start ; end<s.length() ; end++){
-//             char ch = s.charAt(end);
-//             if(ch < '0' || ch > '9'){
-//                 break;
-//             }
-//         }
-//         if(start==end){
-//             return 0;
-//         }
-
-//         String maxString = "2147483647";
-//         String minString = "2147483648";
-//         String result = s.substring(start, end);
-//         if(maxString < result){
-//             return Integer.MAX_VALUE;
-//         }else if (sign == -1 && result < minString){
-//             return Integer.MIN_VALUE;
-//         }
-//         return sign * Integer.parseInt(s.substring(start, end));
-//     }
-// }
-
-// /*
-// 91283472332
-// 2147483647
-// */
+/*
+91283472332
+2147483647
+*/
