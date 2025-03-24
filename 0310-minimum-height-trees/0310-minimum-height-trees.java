955ec6e9c7ec37da32a2a74c18bc5761d5cd717e
taetaetae <taetaetae_@naver.com>
2025-03-25 00:12:58 +0900

    Time: 30 ms (39.93%), Space: 56.5 MB (54.89%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.java
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -1,32 +1,50 @@
 class Solution {
-public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-    if (n == 1) return Collections.singletonList(0);
-
-    List<Set<Integer>> adj = new ArrayList<>(n);
-    for (int i = 0; i < n; ++i) adj.add(new HashSet<>());
-    for (int[] edge : edges) {
-        adj.get(edge[0]).add(edge[1]);
-        adj.get(edge[1]).add(edge[0]);
-    }
-
-    List<Integer> leaves = new ArrayList<>();
-    for (int i = 0; i < n; ++i)
-        if (adj.get(i).size() == 1) leaves.add(i);
-
-    while (n > 2) {
-        n -= leaves.size();
-        List<Integer> newLeaves = new ArrayList<>();
-        for (int i : leaves) {
-            int j = adj.get(i).iterator().next();
-            adj.get(j).remove(i);
-            if (adj.get(j).size() == 1) newLeaves.add(j);
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> result = new ArrayList<>();
+        if(n <= 0){
+            return result;
         }
-        leaves = newLeaves;
-    }
-    return leaves;
-}
+        if(n == 1){
+            result.add(0);
+            return result;
+        }
 
-// Runtime: 53 ms
+        List<Set<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new HashSet<>());
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (adj.get(i).size() == 1) {
+                leaves.add(i);
+            }
+        }
+
+        int remainingNodes = n;
+        while(remainingNodes > 2){
+            remainingNodes -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+
+            for(int leaf: leaves){
+                int neighbor = adj.get(leaf).iterator().next();
+                adj.get(neighbor).remove(leaf);
+                if (adj.get(neighbor).size() == 1) {
+                    newLeaves.add(neighbor);
+                }
+            }
+            leaves = newLeaves;
+        }
+
+        result.addAll(leaves);
+        return result;
+    }
 }
 
 /*
