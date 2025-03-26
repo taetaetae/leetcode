342ba968b993db0dadcd8ab1a09e281e85604638
taetaetae <taetaetae_@naver.com>
2025-03-26 19:04:16 +0900

    Difficulty:Medium, Time:49 ms (40.81%) , Space: 114.9 MB (48.68%)

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,27 +1,32 @@
-public class LRUCache extends LinkedHashMap<Integer, Integer> {
-    private final int capacity;
+class LRUCache {
 
+	int size;
+	LinkedHashMap<Integer, Integer> cache;
+	
     public LRUCache(int capacity) {
-        // 파라미터:
-        //  - initialCapacity: 해시 초기 용량 (capacity 정도로 두면 됨)
-        //  - loadFactor: 해시 테이블 부하율(기본 0.75)
-        //  - accessOrder: true -> 접근 순서로 정렬
-        super(capacity, 0.75f, true);
-        this.capacity = capacity;
+        this.size = capacity;
+        this.cache = new LinkedHashMap(); 
     }
-
+    
     public int get(int key) {
-        // LinkedHashMap의 getOrDefault 이용
-        return super.getOrDefault(key, -1);
+    	if(cache.containsKey(key)) {
+    		int value = cache.get(key);
+    		cache.remove(key);
+    		cache.put(key, value);
+    		return value;
+    	}
+        return -1;
     }
-
+    
     public void put(int key, int value) {
-        super.put(key, value);
-    }
-
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        // size가 capacity보다 커지면 true -> 가장 오래된 entry 자동 제거
-        return this.size() > this.capacity;
+    	if(cache.containsKey(key)) {
+    		cache.remove(key);
+    	}
+    	cache.put(key, value);
+    	if(cache.size() > size) {
+    		int removeKey = cache.keySet().iterator().next();
+    		cache.remove(removeKey);
+    	}
+    	
     }
 }
