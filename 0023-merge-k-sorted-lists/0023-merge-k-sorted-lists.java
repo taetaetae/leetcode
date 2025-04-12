063be10912cfd35a093d1c0875e44770fbea4f4e
taetaetae <taetaetae_@naver.com>
2025-04-12 20:28:39 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,31 +9,24 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists){
-    return partion(lists,0,lists.length-1);
-}
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>( (a,b) -> a.val - b.val );
 
-public static ListNode partion(ListNode[] lists,int s,int e){
-    if(s==e)  return lists[s];
-    if(s<e){
-        int q=(s+e)/2;
-        ListNode l1=partion(lists,s,q);
-        ListNode l2=partion(lists,q+1,e);
-        return merge(l1,l2);
-    }else
-        return null;
-}
+        for(ListNode listNode : lists){
+            while(listNode != null){
+                  System.out.println(listNode.val);
+                pq.add(listNode);
+                listNode = listNode.next;
+            }
+        }
+        System.out.println("---");
 
-//This function is from Merge Two Sorted Lists.
-public static ListNode merge(ListNode l1,ListNode l2){
-    if(l1==null) return l2;
-    if(l2==null) return l1;
-    if(l1.val<l2.val){
-        l1.next=merge(l1.next,l2);
-        return l1;
-    }else{
-        l2.next=merge(l1,l2.next);
-        return l2;
+        ListNode currentNode = pq.poll();
+        ListNode resultNode = currentNode;
+        while(!pq.isEmpty()){
+            currentNode.next = pq.poll();
+            currentNode = currentNode.next;
+        }
+        return resultNode;
     }
-}
 }
